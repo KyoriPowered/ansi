15e8c165878d230e0c906e7d886e60747de2fcd1
Emilia Dreamer <emilia@rymiel.space>
2023-04-10 02:57:47 +0300

    Improve coverage
    Fixes #8

--- src/test/java/net/kyori/ansi/ANSIComponentRendererTest.java
+++ src/test/java/net/kyori/ansi/ANSIComponentRendererTest.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of ansi, licensed under the MIT License.
  *
- * Copyright (c) 2021-2022 KyoriPowered
+ * Copyright (c) 2021-2023 KyoriPowered
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -41,6 +41,80 @@ class ANSIComponentRendererTest {
       r.pushStyle(bold);
       r.text("i'm bold");
       r.popStyle(bold);
+    }));
+  }
+
+  @Test
+  void testColor() {
+    Consumer<ANSIComponentRenderer<TestStyle>> redAction = r -> {
+      final TestStyle red = TestStyle.EMPTY.color(0xff_55_55);
+      r.pushStyle(red);
+      r.text("i'm red");
+      r.popStyle(red);
+    };
+    assertEquals("\u001B[38;2;255;85;85mi'm red\u001b[0m", this.render(redAction, ColorLevel.TRUE_COLOR));
+    assertEquals("\u001B[38;5;203mi'm red\u001b[0m", this.render(redAction, ColorLevel.INDEXED_256));
+    assertEquals("\u001B[91mi'm red\u001b[0m", this.render(redAction, ColorLevel.INDEXED_16));
+
+    Consumer<ANSIComponentRenderer<TestStyle>> pureRedAction = r -> {
+      final TestStyle red = TestStyle.EMPTY.color(0xff_00_00);
+      r.pushStyle(red);
+      r.text("i'm very red");
+      r.popStyle(red);
+    };
+    assertEquals("\u001B[38;2;255;0;0mi'm very red\u001b[0m", this.render(pureRedAction, ColorLevel.TRUE_COLOR));
+    assertEquals("\u001B[38;5;196mi'm very red\u001b[0m", this.render(pureRedAction, ColorLevel.INDEXED_256));
+    // todo downsample
+    // assertEquals("\u001B[91mi'm very red\u001b[0m", this.render(pureRedAction, ColorLevel.INDEXED_16));
+  }
+
+  @Test
+  void testDeepColor() {
+    assertEquals("\u001B[38;2;0;255;0mg\u001B[38;2;0;0;255mb\u001B[38;2;0;255;0mg\u001B[38;2;0;0;255mb\u001B[38;2;0;255;0mg\u001B[38;2;0;0;255mb\u001B[38;2;0;255;0mg\u001B[38;2;0;0;255mb\u001B[38;2;0;255;0mg\u001B[38;2;0;0;255mb\u001B[38;2;0;255;0mg\u001B[38;2;0;0;255mb\u001B[0m", this.render(r -> {
+      final TestStyle g = TestStyle.EMPTY.color(0x00_ff_00);
+      final TestStyle b = TestStyle.EMPTY.color(0x00_00_ff);
+      for (int i = 0; i < 6; i++) {
+        r.pushStyle(g);
+        r.text("g");
+        r.pushStyle(b);
+        r.text("b");
+      }
+      for (int i = 0; i < 6; i++) {
+        r.popStyle(b);
+        r.popStyle(g);
+      }
+    }));
+  }
+
+  @Test
+  void testStringBuilder() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("hello ");
+    final ANSIComponentRenderer.ToStringBuilder<TestStyle> renderer = ANSIComponentRenderer.toStringBuilder(TestStyle.ops(), ColorLevel.TRUE_COLOR);
+    renderer.builder(sb);
+    final TestStyle magenta = TestStyle.EMPTY.color(0xff_55_ff);
+    renderer.pushStyle(magenta);
+    renderer.text("colorful");
+    renderer.popStyle(magenta);
+    renderer.complete();
+    sb.append(" world");
+
+    assertEquals("hello \u001B[38;2;255;85;255mcolorful\u001B[0m world", sb.toString());
+  }
+
+  @Test
+  void testFalseDecoration() {
+    assertEquals("\u001B[1mon\u001B[0moff\u001B[1mon\u001B[0m", this.render(r -> {
+      final TestStyle boldOn = TestStyle.EMPTY.bold(StyleOps.State.TRUE);
+      final TestStyle boldOff = TestStyle.EMPTY.bold(StyleOps.State.FALSE);
+
+      r.pushStyle(boldOn);
+      r.text("on");
+      r.pushStyle(boldOff);
+      r.text("off");
+      r.popStyle(boldOff);
+      r.text("on");
+      r.popStyle(boldOff);
     }));
   }
 
